dbcc27a8f5757ff5edd553c53567906fc510177e
Birulaibe Bola de Fogo <leoschuch23@gmail.com>
2022-11-18 17:57:48 -0300

    navbar, banner n skills

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBar} from './components/NavBar'
+import { Banner} from './components/Banner'
+import { Skills } from './components/Skills';
+import {Projects} from './components/Projects'
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <Skills/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from '../assets/img/header-img.svg'
+
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeletingState] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100)
+
+    const toRotate = ['Javascript', 'React', 'NextJs', 'Typescript'];
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        },delta)
+        return () => {clearInterval(ticker)}
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length + 1)
+
+        setText(updatedText);
+
+        if(isDeleting){
+            setDelta(prevDelta => prevDelta / 2)
+        }
+        if(!isDeleting && updatedText === fullText){
+            setIsDeletingState(true);
+            setDelta(period)
+        } else if(isDeleting && updatedText === ''){
+            setIsDeletingState(false)
+            setLoopNum(loopNum + 1)
+            setDelta(500)
+        }
+    }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Bem-vindo ao meu portifólio</span>
+            <h1>{`Tecnologias que utilizo:`}<span className="wrap"> {text}</span></h1>
+            <p>Me chamo Leonardo Schuch, tenho 30 anos. Sou apaixonado por novas tecnologias e por novos desafios.</p>
+            <button onClick={() => console.log('connect')}>Vamos conectar.<ArrowRightCircle size={25}/></button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt='Headder IMG'/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
